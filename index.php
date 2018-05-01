7c1d6a0ccf868f1cb858291cc50559edfaec9578
Snehil Santhalia <snehil.santhalia@gmail.com>
2018-05-01 20:13:08 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -63,7 +63,7 @@ if(isset($_COOKIE['user']))
             </header>
         </div>
         
-        
+        <!-- User Login Modal -->
         <div id="login_modal" class="w3-modal">
             <div class="w3-modal-content w3-card-4 w3-animate-zoom" style="max-width:600px">
                 <div class="w3-center"><br>
@@ -82,6 +82,32 @@ if(isset($_COOKIE['user']))
                         </select>
                         
                         <br><br><label><b>Username</b></label>
+                        <input class="w3-input w3-margin-bottom" type="text" placeholder="Enter Username" name="username" required>
+                        <label><b>Password</b></label>
+                        <input class="w3-input " type="password" placeholder="Enter Password" name="psw" required>
+                        <input class="w3-check w3-margin-top" type="checkbox" name="remmember" checked="checked"> Remember me
+						<button id = 'login' class="w3-button w3-block w3-teal w3-section w3-padding" name="submit" type="submit">Login</button>
+                        </div>
+                    </form>
+
+                    <div class="w3-container w3-border-top w3-padding-16 w3-light-grey">
+                        <button id = 'cancel' onclick="document.getElementById('login_modal').style.display='none'" type="button" class="w3-button w3-red">Cancel</button>
+                        <span class="w3-right w3-padding w3-hide-small">Forgot <a href="#">password?</a></span>
+                    </div>
+                </div>
+            </div>
+        
+        <!-- Admin Modal -->
+        <div id="admin_modal" class="w3-modal">
+            <div class="w3-modal-content w3-card-4 w3-animate-zoom" style="max-width:600px">
+                <div class="w3-center"><br>
+                    <span onclick="document.getElementById('login_modal').style.display='none'" class="w3-button w3-xlarge w3-transparent w3-display-topright" title="Close Modal">x</span>
+                    <img src="img_avatar.png" alt="Avatar" style="width:30%" class="w3-circle w3-margin-top">
+                </div>
+
+                <form class="w3-container" action="" method="post">
+                    <div class="w3-section">
+                        <label><b>Username</b></label>
                         <input class="w3-input w3-margin-bottom" type="text" placeholder="Enter Username" name="username" required>
                         <label><b>Password</b></label>
                         <input class="w3-input " type="password" placeholder="Enter Password" name="psw" required>
